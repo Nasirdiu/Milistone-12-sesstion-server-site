0b7679a24ff143dee4206c602954d190ef626e86
nasirdiu <nasir.joypurhat@gmail.com>
2022-05-21 11:02:12 +0600

    aa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(
   cors({
     origin: "*",
@@ -25,12 +25,61 @@ async function run() {
   try {
     await client.connect();
     const servicesCollection = client.db("clean-co").collection("service");
+    /*
+      get /get-service => all data
+      post /add-service => create new data
+      put /update-service => modify a data on collection
+      delete /delete-service => delete a data from collection
+    */
 
-    app.get("/service", async (req, res) => {
+    app.get("/get-service", async (req, res) => {
       const services = await servicesCollection.find({}).toArray();
       console.log(services);
       res.send(services);
     });
+
+    app.post("/add-service", async (req, res) => {
+      const data = req.body;
+      const result = await servicesCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.put("/update-service/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = { $set: data };
+      const option = { upsert: true };
+
+      const result = await servicesCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+
+      res.send(result);
+    });
+
+    app.delete("/delete-service/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+
+      res.send(result);
+    });
+
+    //* With try catch block
+
+    // app.post("/add-service", async (req, res) => {
+    //   try {
+    //     const data = req.body;
+    //     const result = await servicesCollection.insertOne(data);
+    //     res.send({ status: true, result: result });
+    //   } catch (error) {
+    //     res.send({ status: false, error });
+    //   }
+    // });
   } finally {
   }
 }
